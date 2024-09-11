e014f05d939cabca40fdce13c30745499ab2bf7d
organichottie <mmatlitech@gmail.com>
2024-09-11 10:50:55 +0200

    API key update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "11cb60e5ofea031ab3tc8e1af4a45bbb";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
